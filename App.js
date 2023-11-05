975c69efcc69dd44679de0ffc0499b3d4190232b
nguyendangthaovy <thaovycan1982002@gmail.com>
2023-11-05 16:36:21 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import screen1 from "./screens/screen1";
+import screen2 from "./screens/screen2";
+
+export default function App() {
+  const stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <stack.Navigator>
+          <stack.Screen name='Home' component={screen1}/>
+          <stack.Screen name='Details' component={screen2}/>
+        </stack.Navigator>
+    </NavigationContainer>
+        
+    
+  );
+}
